3640f161c4e67e22a8a46d65b5e809d73350bd9e
vipulagr890 <gvipul890@gmail.com>
2022-10-26 16:42:08 +0530

    add db connection

---
+++ configs/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userName = "Vipul";
+const password = "EivPzRzTFjPojzR6";
+const cluster = "cluster0";
+const dbName = "drmyqe6";
+
+const dbConnection = () => {
+  const mongoDB = `mongodb+srv://${userName}:${password}@${cluster}.${dbName}.mongodb.net/?retryWrites=true&w=majority`;
+  mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Get the default connection
+  const db = mongoose.connection;
+
+  // Bind connection to error event (to get notification of connection errors)
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  });
+};
+
+export default dbConnection;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dbConnection from "./configs/db.js";
 
 const app = express();
 
@@ -6,6 +7,15 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/signin", (req, res) => {
+  res.send(signin);
+});
+
+app.get("/register", (req, res) => {
+  res.send(register);
+});
+
 app.listen(process.env.PORT || 4200, () => {
+  dbConnection();
   console.log("server has started");
 });
